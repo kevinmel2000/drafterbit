ef07d0c8170b88c2777513418d6ab31f21ad1ae7
egig <egigundari@gmail.com>
2014-11-20 14:26:52 +0700

    clean up

--- content/extensions/system/src/Resources/views/main.php
+++ content/extensions/system/src/Resources/views/main.php
@@ -19,37 +19,6 @@
         ') ?>
         
         <?php echo $this->block('css'); ?>
-
-        <!--<script>
-        (function(){
-          drafTerbit = {
-            baseUrl: "<?php echo base_url() ?>",
-            adminUrl: "<?php echo admin_url() ?>",
-            csrfToken: "<?php echo csrf_token() ?>",
-
-            //replace datatable search box;
-            replaceDTSearch: function(dt) {
-              $('.dataTables_filter').remove();
-
-
-              $(document).on('keydown', "input[type=search]", function(e){
-                var code = e.keyCode || e.which;
-                if (code == 13) {
-                  e.preventDefault();
-                }}
-              );
-
-              //search filter
-              $(document).on('keyup', "input[type=search]", function(e){
-
-                var val = $(this).val();
-                dt.api().search($(this).val()).draw();
-                
-              });
-            }
-          }
-        })();
-        </script>-->
     </head>
 
     <body>
