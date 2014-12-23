7da3892250cdc6d004cb0373bc00bfa2ca47b03b
egig <egigundari@gmail.com>
2014-12-24 05:40:15 +0700

    clean system route

--- content/extensions/system/src/Resources/config/routes.php
+++ content/extensions/system/src/Resources/config/routes.php
@@ -1,46 +1,43 @@
 <?php return [
 
 	'%admin%' => [
-		'methods' => 'get|post',
-		'before' => '@user\Models\Auth::authenticate',
+		'before'    => '@user\Models\Auth::authenticate',
+		'methods'   => 'get|post',
 		'subRoutes' => [
-			'/' => ['controller' => '@system\System::dashboard'],
-			'login' => ['controller' => '@user\Auth::login'],
-			'logout' => ['controller' => '@user\Auth::logout'],
-			'setting' => [
+			'/'         => ['controller' => '@system\System::dashboard'],
+			'login'     => ['controller' => '@user\Auth::login'],
+			'logout'    => ['controller' => '@user\Auth::logout'],
+			'setting'   => [
 				'subRoutes' => [
-					'general' => ['controller' => '@system\Setting::general', 'access' => 'system.change'],
-					'themes' => [
+					'general'   => ['controller' => '@system\Setting::general', 'access' => 'system.change'],
+					'themes'    => [
 						'subRoutes' => [
-							'/' => ['controller' => '@system\Theme::index'],
-							'customize' => ['controller' => '@system\Theme::customize', 'csrf'=>true],
+							'/'              => ['controller' => '@system\Theme::index'],
+							'customize'      => ['controller' => '@system\Theme::customize',     'csrf'=>true],
 							'custom-preview' => ['controller' => '@system\Theme::customPreview', 'csrf'=>true],
 							
-							'widget' => ['controller' => '@system\Widget::index'],
-							'widget/delete' => ['controller' => '@system\Widget::delete'],
-							'menus' => ['controller' => '@system\Menus::index'],
-							'menus/save' => ['controller' => '@system\Menus::save'],
-							'menus/delete' => ['controller' => '@system\Menus::delete'],
+							'widget'         => ['controller' => '@system\Widget::index'],
+							'widget/save'    => ['controller' => '@system\Widget::save', 'methods' => 'post'],
+							'widget/delete'  => ['controller' => '@system\Widget::delete'],
+
+							'menus'          => ['controller' => '@system\Menus::index'],
+							'menus/save'     => ['controller' => '@system\Menus::save', 'methods' => 'posts'],
+							'menus/delete'   => ['controller' => '@system\Menus::delete'],
 						]
-					],
+					]
 					
 				]
 			],
 
 			'system' => [
 				'subRoutes' => [
-					'dashboard' => ['controller' => '@system\System::dashboard'],
-					'log' => ['controller' => '@system\System::log'],
-					'cache' => ['controller' => '@system\System::cache'],
+					'dashboard'     => ['controller' => '@system\System::dashboard'],
+					'log'           => ['controller' => '@system\System::log'],
+					'cache'         => ['controller' => '@system\System::cache'],
 					'drafterbit.js' => ['controller' => '@system\System::drafterbitJs'],
 				],
 			],
 		],
-	],
-
-	'%admin%/setting/themes/widget/save' => [
-		'controller' => '@system\Widget::save',
-		'methods' => 'post'
 	],
 
 	'search' => [
